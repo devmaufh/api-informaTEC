00abbfc2d831ce8e5d7e097ab1b057078ffbc319
Mauricio Flores Hernandez <mau1361317@gmail.com>
2019-11-25 19:31:47 -0600

    twilio removed}

--- controllers/avisos_controller.py
+++ controllers/avisos_controller.py
@@ -34,18 +34,11 @@ class InsertAviso(Resource):
         )
         try:
             aviso_model.save_to_db()
-            # TWILIO MESSAGING
+            titulo = "Tienes un nuevo aviso"
             if (data['prioridad'] == "1"):
-                account_sid = 'ACba644bc2e547cc465bd41308af429c10'
-                auth_token = '12e3a371e7472fc4523ccd85fc4f01eb'
-                client = Client(account_sid, auth_token)
-                message = client.messages \
-                        .create(
-                            body="INFORMATEC: "+data['descripcion'],
-                            from_='+16788661949',
-                            to='+524612180322')
+                titulo "Aviso prioritario"
             url = "https://fcm.googleapis.com/fcm/send"
-            data_to_send = {"to":"/topics/all","notification":{"title": "Tienes un nuevo aviso","body":data['titulo']}}
+            data_to_send = {"to":"/topics/all","notification":{"title": titulo,"body":data['titulo']}}
             auth_head = {"Authorization": "key=AAAAQLWUJi0:APA91bFouaJIsigELqjh-CIzVVzKI3snTkXNvcMIgVC6_wJ9p56w3YK4Gxa-xG6xAzv7Bri3EerYjztbW9lo31ZW0z5kYvax6Iy_TmSR74NXiJW0oe7x3gkeAMSy1agg1s5K3iPZUjaZ","Content-Type" : "application/json"}
             x = requests.post(url, json = data_to_send, headers=auth_head)
             return {
